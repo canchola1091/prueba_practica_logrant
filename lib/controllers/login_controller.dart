25feb6e452817c9544612e3806234e1f0c27edbc
Eduardo Canchola <canchola700@gmail.com>
2025-04-09 19:34:17 -0600

    logica de formulario de login y registro

--- lib/controllers/login_controller.dart
+++ lib/controllers/login_controller.dart
@@ -1,4 +1,6 @@
 
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 import 'package:get/get_state_manager/get_state_manager.dart';
@@ -6,12 +8,16 @@ import 'package:get/get_state_manager/get_state_manager.dart';
 class LoginController  extends GetxController{
 
   bool isLogin = true;
+  bool showPasswordLogin = false;
+  bool showPasswordRegister = false;
 
   //? CONTROLLERS LOGIN
+  final GlobalKey<FormState> loginKey = GlobalKey<FormState>();
   final TextEditingController emailCtrl = TextEditingController();
   final TextEditingController passwordCtrl = TextEditingController();
 
   //? CONTROLLERS REGISTRO
+  final GlobalKey<FormState> registerKey = GlobalKey<FormState>();
   final TextEditingController nameCtrl = TextEditingController();
   final TextEditingController emailRegCtrl = TextEditingController();
   final TextEditingController passwordRegCtrl = TextEditingController();
@@ -19,7 +25,34 @@ class LoginController  extends GetxController{
 
   void chanceViewLoginRegister() {
     isLogin = !isLogin;
-    update(['login']);
+    if(isLogin) {
+      emailCtrl.clear();
+      passwordCtrl.clear();
+    } else {
+      nameCtrl.clear();
+      emailRegCtrl.clear();
+      passwordRegCtrl.clear();
+      phoneCtrl.clear();
+    }
+    update(['login_view']);
+  }
+
+  void changeViewPasswordLogin() {
+    showPasswordLogin = !showPasswordLogin;
+    update(['login_form']);
+  }
+
+  void changeViewPasswordRegister() {
+    showPasswordRegister = !showPasswordRegister;
+    update(['register_form']);
+  }
+
+  void validateLoginForm() {
+    if(loginKey.currentState!.validate()) {
+      log('VALIDO');
+    } else {
+      log('NO VALIDO');
+    }
   }
 
 
---
+++ lib/widgets/register/register_form.dart
@@ -0,0 +1,72 @@
+
+import 'package:flutter/material.dart';
+
+import 'package:get/get_state_manager/get_state_manager.dart';
+
+import 'package:test_logrant/controllers/login_controller.dart';
+import 'package:test_logrant/validations/validations.dart';
+import 'package:test_logrant/widgets/shared/custom_text_form_field.dart';
+
+class RegisterForm extends StatelessWidget {
+  const RegisterForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetBuilder<LoginController>(
+      id: 'register_form',
+      builder: (gx) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0),
+          child: Form(
+            key: gx.registerKey,
+            child: Column(
+              children: [
+                CustomTextFormField(
+                  ctrl:  gx.nameCtrl,
+                  label: 'Nombre',
+                  isLogin: false,
+                  capitalization: TextCapitalization.words,
+                  validation: (value) => Validations.validNames(value!),
+                ),
+                CustomTextFormField(
+                  ctrl:  gx.phoneCtrl,
+                  label: 'Teléfono',
+                  isLogin: false,
+                  inputType: TextInputType.phone,
+                  validation: (value) => Validations.validRequiredAndLength(value!, 10),
+                ),
+                CustomTextFormField(
+                  ctrl:  gx.emailRegCtrl,
+                  label: 'Correo electrónico',
+                  isLogin: false,
+                  inputType: TextInputType.emailAddress,
+                  validation: (value) => Validations.validEmail(value!),
+                ),
+                CustomTextFormField(
+                  ctrl:  gx.passwordRegCtrl,
+                  label: 'Contraseña',
+                  isLogin: false,
+                  isPassword: gx.showPasswordRegister,
+                  validation: (value) => Validations.validRequiredAndLength(value!, 8),
+                  iconSuffix: IconButton(
+                    onPressed: () => gx.changeViewPasswordRegister(),
+                    icon: Icon(
+                      (gx.showPasswordRegister)
+                      ? Icons.visibility
+                      : Icons.visibility_off,
+                      color: Colors.purple,
+                    )
+                  )
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Registrar'),
+                )
+              ]
+            )
+          ),
+        );
+      }
+    );
+  }
+}
